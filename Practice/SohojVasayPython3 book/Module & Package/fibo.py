c4aeef9658fec40f6fc54256012fe80dc5f210e9
shahincse98 <32039099+shahincse98@users.noreply.github.com>
2019-03-03 21:14:45 +0600

    Python 3

---
+++ Practice/SohojVasayPython3 book/Module & Package/fibo.py
@@ -0,0 +1,11 @@
+def fib(n):
+    series = []
+    a, b = 0,1
+    while b < n:
+        series.append(b)
+        a, b = b, a + b
+    return series
+
+if __name__ == "__main__":
+    temp = fib(100)
+    print(temp)
