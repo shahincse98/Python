88f808922b2213ac4f27c95ee9ac86d638f0c4f0
shahincse98 <32039099+shahincse98@users.noreply.github.com>
2019-03-03 21:24:14 +0600

    Python 3

---
+++ Practice/pybook2/3. Object & Class/1.py
@@ -0,0 +1,10 @@
+class Car:
+    name = "Premio"
+    color = "White"
+
+    def start():
+        print("Starting the engine")
+
+print("Name of the car:", Car.name)
+print("Color:", Car.name)
+Car.start()
